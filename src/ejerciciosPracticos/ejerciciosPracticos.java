36c189ccc3fa984961417f20eb6eb223fc9dd986
Rebeca Heredia <rbkheredia90@gmail.com>
2023-05-31 20:10:56 -0300

    suma de valores con limite

--- src/ejerciciosPracticos/ejerciciosPracticos.java
+++ src/ejerciciosPracticos/ejerciciosPracticos.java
@@ -1,5 +1,7 @@
 package ejerciciosPracticos;
 
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -16,7 +18,19 @@ public class ejerciciosPracticos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("prueba");
+        //ejercicio10
+//        Escriba un programa en el cual se ingrese un valor límite positivo, y a continuación solicite
+//números al usuario hasta que la suma de los números introducidos supere el límite inicial.
+        Scanner read = new Scanner(System.in);
+        System.out.println("Ingrese un valor limite");
+        int limit = read.nextInt();
+        int num, total = 0;
+        while(total <limit){
+            System.out.println("Ingrese un valor a sumar");
+            num = read.nextInt();
+            total = total + num;
+        }
+        System.out.println("El limite" +limit + "ha sido superado. El total es: " +total);
     }
     
 }
