419068034bbc0d9348fde30b825cdb3f1d10b3bf
Rebeca Heredia <rbkheredia90@gmail.com>
2023-05-31 19:37:04 -0300

    par o impar

--- src/ejerciciosPracticos/ejerciciosPracticos.java
+++ src/ejerciciosPracticos/ejerciciosPracticos.java
@@ -1,5 +1,7 @@
 package ejerciciosPracticos;
 
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -16,7 +18,19 @@ public class ejerciciosPracticos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("prueba");
+        Scanner read = new Scanner(System.in);
+        //ejercicio6
+//        Crear un programa que dado un numero determine si es par o impar.
+        
+        System.out.println("Ingrese un numero para saber si es par o impar");
+        int number = read.nextInt();
+        parOImpar(number);
     }
-    
+    public static void parOImpar(int number){
+        if(number%2== 0){
+            System.out.println("El numero es par");
+        } else {
+            System.out.println("El numero es impar");
+        }
+    }
 }
