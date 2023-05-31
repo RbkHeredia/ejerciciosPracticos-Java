ba455817c6f15367a465e6d81e6f2435ffccb6ce
Rebeca Heredia <rbkheredia90@gmail.com>
2023-05-31 19:52:53 -0300

    comparar primera letra

--- src/ejerciciosPracticos/ejerciciosPracticos.java
+++ src/ejerciciosPracticos/ejerciciosPracticos.java
@@ -1,5 +1,7 @@
 package ejerciciosPracticos;
 
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -16,7 +18,21 @@ public class ejerciciosPracticos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("prueba");
+        Scanner read = new Scanner(System.in);
+        //ejercicio9
+//        Escriba un programa que pida una frase o palabra y valide si la primera letra de esa frase
+//es una ‘A’. Si la primera letra es una ‘A’, se deberá de imprimir un mensaje por pantalla que
+//diga “CORRECTO”, en caso contrario, se deberá imprimir “INCORRECTO”. Nota: investigar
+//la función Substring y equals() de Java.
+        System.out.println("Ingrese una palabra o frase");
+        String phrase = read.nextLine();
+        String firstLetter = phrase.substring(0,1);
+        System.out.println(firstLetter);
+        if (firstLetter.equals("a")){
+            System.out.println("Correcto");
+        } else {
+            System.out.println("Incorrecto");
+        }
     }
     
 }
