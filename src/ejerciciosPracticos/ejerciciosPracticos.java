b9ac6f9bfade6bf5bb7cc50c9fbb31761098b901
Rebeca Heredia <rbkheredia90@gmail.com>
2023-05-31 19:42:06 -0300

    comparacion string

--- src/ejerciciosPracticos/ejerciciosPracticos.java
+++ src/ejerciciosPracticos/ejerciciosPracticos.java
@@ -1,5 +1,7 @@
 package ejerciciosPracticos;
 
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -16,7 +18,20 @@ public class ejerciciosPracticos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("prueba");
+        //ejercicio7
+//        Crear un programa que pida una frase y si esa frase es igual a “eureka” el programa
+//pondrá un mensaje de Correcto, sino mostrará un mensaje de Incorrecto. Nota: investigar
+//la función equals() en Java.
+        Scanner read = new Scanner(System.in);
+        System.out.println("Ingrese una frase");
+        String phrase = read.nextLine();
+        compare(phrase);
     }
-    
+    public static void compare(String phrase){
+        if(phrase.equals("eureka")){
+            System.out.println("Correcto");
+        } else {
+            System.out.println("Incorrecto");
+        }
+    }
 }
