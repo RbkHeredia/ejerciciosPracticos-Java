bd8c32b28b48bdfb28e614571d2945da57567e43
Rebeca Heredia <rbkheredia90@gmail.com>
2023-05-30 21:28:19 -0300

    ejercicio1 resuelto

--- src/ejerciciosPracticos/ejerciciosPracticos.java
+++ src/ejerciciosPracticos/ejerciciosPracticos.java
@@ -1,3 +1,5 @@
+package ejerciciosPracticos;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,13 +10,22 @@
  *
  * @author rebek
  */
+import java.util.Scanner;
 public class ejerciciosPracticos {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        //ejercicio1
+        //Escribir un programa que pida dos números enteros por teclado y calcule la suma de los
+        //dos. El programa deberá después mostrar el resultado de la suma
+        
+        Scanner read = new Scanner(System.in);
+        int  num1, num2;
+        System.out.println("Ingrese un numero");
+        num1 = read.nextInt();
+        System.out.println("Ingrese otro numero");
+        num2 = read.nextInt();
+        System.out.println("La suma de los numeros es: " + (num1+num2));
     }
     
 }
