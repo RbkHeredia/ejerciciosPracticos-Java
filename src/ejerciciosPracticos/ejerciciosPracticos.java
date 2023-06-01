d4e68d8d82e3a5392a6ac50baeec96b19c5cbff1
Rebeca Heredia <rbkheredia90@gmail.com>
2023-05-31 21:08:41 -0300

    cuadrado de asteriscos

--- src/ejerciciosPracticos/ejerciciosPracticos.java
+++ src/ejerciciosPracticos/ejerciciosPracticos.java
@@ -1,5 +1,7 @@
 package ejerciciosPracticos;
 
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -16,7 +18,26 @@ public class ejerciciosPracticos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("prueba");
+        //ejercicio13
+//        Dibujar un cuadrado de N elementos por lado utilizando el carácter “*”. Por ejemplo, si el
+//cuadrado tiene 4 elementos por lado se deberá dibujar lo siguiente:
+//* * * *
+//*      *
+//*      *
+//* * * *
+        Scanner read = new Scanner(System.in);
+        System.out.println("Ingrese la cantidad de elementos en cada lado");
+        int n = read.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(i == 0 || i == n-1 || j == 0 || j == n-1){
+                    System.out.print("*");
+                } else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
+        }
     }
     
 }
