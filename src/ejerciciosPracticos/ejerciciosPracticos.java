27dd55c673f4a2bbbbfd6a8ccc35e37b31509ef1
Rebeca Heredia <rbkheredia90@gmail.com>
2023-06-05 20:31:18 -0300

    contador de digitos

--- src/ejerciciosPracticos/ejerciciosPracticos.java
+++ src/ejerciciosPracticos/ejerciciosPracticos.java
@@ -1,5 +1,7 @@
 package ejerciciosPracticos;
 
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -16,7 +18,29 @@ public class ejerciciosPracticos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("prueba");
+        //ejercicio17
+//        Recorrer un vector de N enteros contabilizando cuántos números son de 1 dígito, cuántos
+//de 2 dígitos, etcétera (hasta 5 dígitos).
+        Scanner read = new Scanner(System.in);
+        System.out.println("Ingresa la cantidad de valores que deseas ver");
+        int q = read.nextInt();
+        int vector[] = new int[q];
+        for (int i = 0; i < q; i++) {
+            vector[i] = (int) (Math.random()*25000);
+            System.out.println(vector[i] + " - ");
+        }
+        counter(vector, q);
     }
-    
+    public static void counter(int vector[], int q){
+        int counter, rest;
+        for (int i = 0; i < q; i++) {
+            rest = vector[i];
+            counter = 1;
+            while (rest >= 10){
+                rest = rest /10;
+                counter++;
+            }
+            System.out.println("cantidad de digitos: " + counter);
+        }
+    }
 }
