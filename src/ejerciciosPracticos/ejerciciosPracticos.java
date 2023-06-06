9d36a5e9d4a20b25d7ea32fb7df9badc4c655a25
Rebeca Heredia <rbkheredia90@gmail.com>
2023-06-05 21:21:52 -0300

    matriz transpuesta

--- src/ejerciciosPracticos/ejerciciosPracticos.java
+++ src/ejerciciosPracticos/ejerciciosPracticos.java
@@ -16,7 +16,30 @@ public class ejerciciosPracticos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("prueba");
+        //ejercicio18
+//        Realizar un programa que rellene un matriz de 4 x 4 de valores aleatorios y muestre la
+//traspuesta de la matriz. La matriz traspuesta de una matriz A se denota por B y se obtiene
+//cambiando sus filas por columnas (o viceversa).
+        System.out.println("Matriz");
+        int matriz [][] = new int [3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                matriz[i][j] = (int)(Math.random()*10);
+                System.out.print("[" + matriz[i][j] + "]");
+            }
+            System.out.println("");
+        }
+        System.out.println("--------------------------------------");
+        transposed(matriz);
+    }
+    
+    public static  void transposed(int matriz[][]){
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print("[" + matriz[j][i] + "]");
+            }
+            System.out.println("");
+        }
     }
     
 }
