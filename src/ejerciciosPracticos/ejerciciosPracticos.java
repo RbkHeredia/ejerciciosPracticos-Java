50d5839f6507d69cc45ad9baa4189dade921b0d0
Rebeca Heredia <rbkheredia90@gmail.com>
2023-06-06 21:10:23 -0300

    buscar matriz dentro de matriz

--- src/ejerciciosPracticos/ejerciciosPracticos.java
+++ src/ejerciciosPracticos/ejerciciosPracticos.java
@@ -16,7 +16,56 @@ public class ejerciciosPracticos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("prueba");
+        //ejercicio21
+        int matriz10[][] = new int[10][10];
+        int matriz3[][] = new int[3][3];
+        boolean validacion=false;
+        System.out.println("Matriz 10 x 10");
+        matriz10 = generarMatriz(10);
+        System.out.println("------------------");
+        System.out.println("Matriz 3 x 3");
+        matriz3 = generarMatriz(3);
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                validacion= validar(matriz10, matriz3, i, j);
+                if(validacion){
+                    System.out.println("Se encontro una coincidencia en la fila "+ i +", columna "+j);
+                }
+            }
+        }
     }
     
+    public static int[][] generarMatriz(int num){
+        int matriz[][] = new int[num][num];
+        for (int i = 0; i < num; i++) {
+            for (int j = 0; j < num; j++) {
+                matriz[i][j] = (int)(Math.random()*2);
+                if(matriz[i][j]<10){
+                    System.out.print("[0"+matriz[i][j]+"]");
+                } else{
+                    System.out.print("["+matriz[i][j]+"]");
+                }
+                
+            }
+            System.out.println("");
+        }
+        return matriz;
+    }
+    
+    public static boolean validar(int matriz10[][], int matriz3[][], int fila, int col){
+        
+        int counter = 0;
+        boolean exist = false;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if(matriz3[i][j]== matriz10[i+fila][j+col]){
+                    counter = counter +1;
+                    if (counter == 9){
+                        exist = true;
+                    } 
+                }
+            }
+        }
+        return exist;
+    }
 }
